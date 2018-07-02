f5a8079e08908cea7747b28f574ad33b971c27b2
armandoherrer107 <>
2018-07-02 08:33:52 -0600

    Agregando clases

---
+++ src/clases/Metodologia.java
@@ -0,0 +1,36 @@
+
+package clases;
+
+public class Metodologia {
+    
+    private String idMetodologia;
+    private String metodologia;
+    private String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIdMetodologia() {
+        return idMetodologia;
+    }
+
+    public void setIdMetodologia(String idMetodologia) {
+        this.idMetodologia = idMetodologia;
+    }
+
+    public String getMetodologia() {
+        return metodologia;
+    }
+
+    public void setMetodologia(String metodologia) {
+        this.metodologia = metodologia;
+    }
+    
+   
+    
+}
